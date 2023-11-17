8dc41cea0fd5345896d14cac81be56cece1e8401
SEUNGJUNHH <103011980+SEUNGJUNHH@users.noreply.github.com>
2023-11-17 23:42:06 +0900

    [feature]오늘의 기분 기능 구조
    Q파일 푸시를 위한 중간 커밋입니다.

---
+++ src/main/java/com/neighborhood/domain/todaymood/controller/TodayMoodController.java
@@ -0,0 +1,11 @@
+package com.neighborhood.domain.todaymood.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("todaymood")
+public class TodayMoodController {
+}
