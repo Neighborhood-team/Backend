f873ecd5ae8f964a1322cf0d184c531760dd8a6c
SeChang Jeong <ckd0325@gmail.com>
2023-11-24 17:35:40 +0900

    Update: 오늘의 질문 api Principal 사용하도록 변경
    Related to #39

--- src/main/java/com/neighborhood/domain/todayquestion/controller/TodayQuestionApiController.java
+++ src/main/java/com/neighborhood/domain/todayquestion/controller/TodayQuestionApiController.java
@@ -9,50 +9,52 @@ import com.neighborhood.domain.todayquestion.service.TodayQuestionApiService;
 import com.neighborhood.global.exception.RestApiException;
 import com.neighborhood.global.exception.errorCode.CommonErrorCode;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.Principal;
 import java.time.LocalDate;
 
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 public class TodayQuestionApiController implements TodayQuestionApi {
 
     private final MemberRepository memberRepository;
     private final TodayQuestionApiService todayQuestionApiService;
 
-    public UpdateTodayQuestionDto updateTodayQuestion(Long memberId) {
+    public UpdateTodayQuestionDto updateTodayQuestion(Principal principal) {
 
-        Member member = memberRepository
-                .findById(memberId).orElseThrow(() -> new RestApiException(CommonErrorCode.RESOURCE_NOT_FOUND));
+        Member member = memberRepository.findById(Long.parseLong(principal.getName()))
+                .orElseThrow(() -> new RestApiException(CommonErrorCode.RESOURCE_NOT_FOUND));
         Boolean isUpdate = todayQuestionApiService.checkUpdate(member);
-
         String message = isUpdate ? "오늘의 질문이 갱신 되었습니다." : "오늘의 질문이 유지됩니다.";
         return new UpdateTodayQuestionDto(message);
     }
 
 
-    public TodayQuestionDto.TodayQuestion getTodayQuestion(Long memberId) {
+    public TodayQuestionDto.TodayQuestion getTodayQuestion(Principal principal) {
 
-        Member member = memberRepository
-                .findById(memberId).orElseThrow(() -> new RestApiException(CommonErrorCode.RESOURCE_NOT_FOUND));
+        Member member = memberRepository.findById(Long.parseLong(principal.getName()))
+                .orElseThrow(() -> new RestApiException(CommonErrorCode.RESOURCE_NOT_FOUND));
 
         return todayQuestionApiService.getTodayQuestion(member);
     }
 
-    public ResponseEntity<?> addAnswer(Long memberId, TodayQuestionDto.AnswerForm body) {
+    public ResponseEntity<?> addAnswer(Principal principal, TodayQuestionDto.AnswerForm body) {
 
-        Member member = memberRepository
-                .findById(memberId).orElseThrow(() -> new RestApiException(CommonErrorCode.RESOURCE_NOT_FOUND));
+        Member member = memberRepository.findById(Long.parseLong(principal.getName()))
+                .orElseThrow(() -> new RestApiException(CommonErrorCode.RESOURCE_NOT_FOUND));
 
         return todayQuestionApiService.addAnser(member, body);
     }
 
-    public TodayQuestionDto.AnswersOfFamily getFamilyAnswers(Long memberId, LocalDate date) {
+    public TodayQuestionDto.AnswersOfFamily getFamilyAnswers(Principal principal, LocalDate date) {
 
-        Member member = memberRepository
-                .findById(memberId).orElseThrow(() -> new RestApiException(CommonErrorCode.RESOURCE_NOT_FOUND));
+        Member member = memberRepository.findById(Long.parseLong(principal.getName()))
+                .orElseThrow(() -> new RestApiException(CommonErrorCode.RESOURCE_NOT_FOUND));
 
         return todayQuestionApiService.getFamilyAnswers(member, date);
     }
