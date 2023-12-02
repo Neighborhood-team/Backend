3e78359824e394209bba810bcb6a13675960b02d
SEUNGJUNHH <103011980+SEUNGJUNHH@users.noreply.github.com>
2023-12-02 15:27:53 +0900

    [fix] 문장복사기 http메서드 수정
    get메서드에서 post로 변경

--- src/main/java/com/neighborhood/domain/contextcopy/controller/ContextCopyApi.java
+++ src/main/java/com/neighborhood/domain/contextcopy/controller/ContextCopyApi.java
@@ -11,10 +11,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
 @Validated
 public interface ContextCopyApi {
@@ -24,7 +21,7 @@ public interface ContextCopyApi {
             @ApiResponse(responseCode = "401", description = "복사할 문장 조회 실패"),
             @ApiResponse(responseCode = "404", description = "Not Found", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorSchema.class))),
             @ApiResponse(responseCode = "500", description = "서버 내부 에러", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorSchema.class)))})
-    @GetMapping(produces = {"application/json"})
+    @PostMapping(produces = {"application/json"})
     ResponseEntity<ResponseApiMessage> getContext(
             @Parameter(in = ParameterIn.DEFAULT, description = "사용자 가족 유형", required = true, schema = @Schema()) @RequestBody String familyType);
 }
