26a712d078b400665e07984ed124191ec7bf8b88
SeChang Jeong <ckd0325@gmail.com>
2023-12-05 21:34:37 +0900

    Fix: 스케줄 조회 시 오늘 날짜인 일정 조회안되는 케이스 수정
    Related to #71

--- src/main/java/com/neighborhood/domain/profile/service/ScheduleService.java
+++ src/main/java/com/neighborhood/domain/profile/service/ScheduleService.java
@@ -48,7 +48,7 @@ public class ScheduleService {
         List<Schedule> before =
                 schduleRepository.findByMemberAndEndDateBeforeOrderByStartDateAsc(member, LocalDate.now());
         List<Schedule> after =
-                schduleRepository.findByMemberAndEndDateAfterOrderByStartDateAsc(member, LocalDate.now());
+                schduleRepository.findByMemberAndEndDateAfterOrEndDateEqualsOrderByStartDateAsc(member, LocalDate.now());
         List<ScheduleDto.InquiryForm> beforeDtos = new ArrayList<>();
         List<ScheduleDto.InquiryForm> afterDtos = new ArrayList<>();
 
