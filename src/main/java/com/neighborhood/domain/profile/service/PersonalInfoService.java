7dc89ecc6479a3777d32f352ba1fa98224aa6c6b
DaWoonKang <nachokang@ajou.ac.kr>
2023-12-06 23:29:27 +0900

    Chore: 개인정보에 3가지 고정 정보 추가
    Related to: #84

--- src/main/java/com/neighborhood/domain/profile/service/PersonalInfoService.java
+++ src/main/java/com/neighborhood/domain/profile/service/PersonalInfoService.java
@@ -1,20 +1,33 @@
 package com.neighborhood.domain.profile.service;
 
+import com.neighborhood.domain.family.entity.Family;
+import com.neighborhood.domain.family.repository.FamilyRepository;
+import com.neighborhood.domain.login.jwt.TokenProvider;
+import com.neighborhood.domain.member.entity.FamilyRole;
 import com.neighborhood.domain.member.entity.Member;
 import com.neighborhood.domain.member.repository.MemberRepository;
 import com.neighborhood.domain.profile.dto.PersonalInfoInputDto;
 import com.neighborhood.domain.profile.dto.PersonalInfoResponseDto;
 import com.neighborhood.domain.profile.entity.PersonalInfo;
 import com.neighborhood.domain.profile.repository.PersonalInfoRepository;
+import com.neighborhood.global.exception.RestApiException;
+import com.neighborhood.global.exception.errorCode.MemberErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class PersonalInfoService {
     private final PersonalInfoRepository personalInfoRepository;
     private final MemberRepository memberRepository;
+    private final TokenProvider tokenProvider;
+    private final FamilyRepository familyRepository;
+    private final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 
 
     public Member findMember(Long memberId) {
@@ -27,16 +40,37 @@ public class PersonalInfoService {
                 .orElse(null);
     }
 
+    public Family findFamily(String familyCode) {
+        return familyRepository.findByFamilyCode(familyCode)
+                .orElseThrow(() -> new IllegalArgumentException("가족 코드를 재확인하세요!"));
+    }
+
     @Transactional
     public PersonalInfoResponseDto updatePersonalInfo(Long memberId, PersonalInfoInputDto personalInfoInputDto) {
         Member member = findMember(memberId);
         Long foundPersonalInfoId = member.getPersonalInfo().getPersonalInfoId();
+
+        Family family = findFamily(member.getFamily().getFamilyCode());
+
 
         PersonalInfo personalInfo = findPersonalInfo(foundPersonalInfoId);
         personalInfo.updatePersonalInfo(personalInfoInputDto.getMbti(), personalInfoInputDto.getBloodType(), personalInfoInputDto.getLikes(),
                 personalInfoInputDto.getHates(), personalInfoInputDto.getInterests(), personalInfoInputDto.getMotto(),
                 personalInfoInputDto.getRoleInFam(), personalInfoInputDto.getPlaceToTrip(), personalInfoInputDto.getBirthdayGift(),
                 personalInfoInputDto.getMySizes());
+
+        List<Member> members = family.getMembers();
+        for(Member m : members) {
+            if(m.getFamilyRole() == null) break;
+            if(personalInfoInputDto.getFamilyRole().equals(FamilyRole.DAD) && m.getFamilyRole().equals(FamilyRole.DAD)) {
+                throw new RestApiException(MemberErrorCode.DAD_ALREADY_EXISTS);
+            }
+            else if(personalInfoInputDto.getFamilyRole().equals(FamilyRole.MOM) && m.getFamilyRole().equals(FamilyRole.MOM)) {
+                throw new RestApiException(MemberErrorCode.MOM_ALREADY_EXISTS);
+            }
+        }
+        member.updateFixedInfo(personalInfoInputDto.getName(), personalInfoInputDto.getFamilyRole(), LocalDate.parse(personalInfoInputDto.getBirthDate(), dateTimeFormatter));
+
 
         return new PersonalInfoResponseDto(personalInfo);
     }
